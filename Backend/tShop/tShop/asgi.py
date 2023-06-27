eb6499733dc330e60ba4615edc5b9d6efc98377d
anthonynarine <anarine83@gmail.com>
2023-06-26 21:04:32 -0400

    corsheaders, rest_framework and rest_framewrok.authtoken apps added + REST_FRAMEWORK dict with default auth and default permissions classes added to setting.py

--- Backend/tShop/tShop/asgi.py
+++ Backend/tShop/tShop/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for tshop project.
+ASGI config for tShop project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tshop.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tShop.settings")
 
 application = get_asgi_application()
