20aee31c67c462aeab6b31f83fa77c47f011a90f
anthonynarine <anarine83@gmail.com>
2023-07-06 11:28:21 -0400

    custom user model create username field overidden, user will now be identifed by email and not username

--- backend/ecom/api/user/models.py
+++ backend/ecom/api/user/models.py
@@ -1,3 +1,23 @@
+from unittest.util import _MAX_LENGTH
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    name = models.CharField(max_length=50, default="Anonymous")
+    email = models.EmailField(max_length=100, unique=True)
+    
+    #user field is overidden. used as default for base user
+    username = None
+    
+    #identify user by email not username
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    gender = models.CharField(max_length=10, blank=True, null=True)
+    
+    session_token = models.CharField(max_lenght=10, defaul=0)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
