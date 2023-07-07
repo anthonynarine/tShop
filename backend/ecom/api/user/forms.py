514fae994ac5ff77dd5f885573bd967687393bcf
anthonynarine <anarine83@gmail.com>
2023-07-07 07:03:59 -0400

    forms.py created here we subclass UserCreationForm and UserChagneForm so they use the new CustomUser model + told admin to use the forms by subclassing UserAdmin in user/admin.py

---
+++ backend/ecom/api/user/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
+from .models import CustomUser
+
+"""
+make sure to Tell the admin to use these forms
+ by subclassing UserAdmin in users/admin.py:"""
+class CustomUserCreationForm(UserCreationForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = CustomUser
+        fields = ("email",)
