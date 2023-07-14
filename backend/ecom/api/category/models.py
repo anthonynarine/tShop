f62e2186ab84634eb8715fae8a9cfee66eabd9cf
anthonynarine <anarine83@gmail.com>
2023-07-13 23:59:50 -0400

    request to fect products from the api and update state setup in home component

--- backend/ecom/api/category/models.py
+++ backend/ecom/api/category/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=250)
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
--- frontend/shop/src/core/Home.jsx
+++ frontend/shop/src/core/Home.jsx
@@ -1,29 +1,43 @@
 import React from "react";
-import { useState, useEffect } from "react"
-import { getProducts } from "./helper/coreapiCalls";
+import { useState, useEffect } from "react";
+import { baseUrl } from "./shared";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
 
-    const [products, setProducts] = useState([]);
-    const [error, setError] = useState(false);
-
-    const loadAllProducts = ()=> {
-        getProducts()
-        .then(data =>{
-            if (data.error){
-                setError(data.error)
-                console.log(error)
-            }else{
-                setProducts(data)
-            }
-        });
-    };
-
+  useEffect(() => {
+    console.log("fetching...");
+    // fetch products from the api and update state
+    async function fetchProducts() {
+      try {
+        const url = `${baseUrl}api/products/`;
+        const response = await fetch(url,);
+        const data = await response.json();
+        console.log(data);
+        setProducts(data);
+      } catch (error) {
+        setError(error)
+        console.log("Error:", error)
+      }
+    }
     
+    fetchProducts();
+  }, []);
+
+  if (error){
+    return <div>Error: {error.message}</div>
+  }
 
   return (
-    <div>
-         <h1>Home component under construction </h1>
-    </div>
+    <>
+      {products ? (
+        products.map((product) => {
+          return <div>{product.name}</div>;
+        })
+      ) : (
+        <p>No data available</p>
+      )}
+    </>
   );
 }
